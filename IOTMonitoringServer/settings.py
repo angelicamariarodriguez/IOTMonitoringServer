b4d367e1e85b59ceaab376a945143f13370632c6
Angelica <am.rodriguez12@uniandes.edu.co>
2022-09-26 18:12:44 -0500

    Se reconfigruan ips

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "3.89.192.54"]
+ALLOWED_HOSTS = ["localhost", "54.161.169.220"]
 
 
 # Application definition
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "54.147.114.251", 
+        "HOST": "54.224.39.241", 
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "34.228.238.217"
+MQTT_HOST = "50.19.135.154"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
