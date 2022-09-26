683c72d3c580cc8b99348f1407e3cf344146cf44
Angelica <am.rodriguez12@uniandes.edu.co>
2022-09-25 20:15:51 -0500

    se ajusta cdigo

--- control/monitor.py
+++ control/monitor.py
@@ -58,50 +58,7 @@ def analyze_data():
     print(len(aggregation), "dispositivos revisados")
     print(alerts, "alertas enviadas")
 
-def analyze_data_2():
-    # Consulta todos los datos de la última hora, los agrupa por estación y variable
-    # Compara el promedio con el valor maximo
-    # Si el promedio se excede el valor maximo, se envia un mensaje de alerta.
 
-    print("Calculando alertas...")
-
-    data = Data.objects.filter(
-        base_time__gte=datetime.now() - timedelta(hours=1))
-    aggregation = data.annotate(check_value=Max('max_value')) \
-        .select_related('station', 'measurement') \
-        .select_related('station__user', 'station__location') \
-        .select_related('station__location__city', 'station__location__state',
-                        'station__location__country') \
-        .values('check_value', 'station__user__username',
-                'measurement__name',
-                'measurement__max_value',
-                'station__location__city__name',
-                'station__location__state__name',
-                'station__location__country__name')
-    alerts = 0
-    for item in aggregation:
-        alert = False
-
-        variable = item["measurement__name"]
-        max_value = item["measurement__max_value"] or 0
-
-        country = item['station__location__country__name']
-        state = item['station__location__state__name']
-        city = item['station__location__city__name']
-        user = item['station__user__username']
-
-        if item["check_value"] > max_value :
-            alert = True
-
-        if alert:
-            message = "ALERT {} {} {}".format(variable,max_value)
-            topic = '{}/{}/{}/{}/in'.format(country, state, city, user)
-            print(datetime.now(), "Sending alert to {} {}".format(topic, variable))
-            client.publish(topic, message)
-            alerts += 1
-
-    print(len(aggregation), "dispositivos revisados")
-    print(alerts, "alertas enviadas")
 
 def on_connect(client, userdata, flags, rc):
     '''
@@ -151,8 +108,6 @@ def start_cron():
     print("Iniciando cron...")
     schedule.every(5).minutes.do(analyze_data)
     print("Iniciando cron...")
-    schedule.every(5).minutes.do(analyze_data_2)
-    print("Servicio de control iniciado")
     while 1:
         schedule.run_pending()
         time.sleep(1)
